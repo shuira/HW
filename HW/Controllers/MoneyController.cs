c2d2b13834500451f66a605f81050fdc4ad5fbfc
shuira <shuira@gmail.com>
2018-07-22 20:27:32 +0800

    待修改

--- HW/Controllers/MoneyController.cs
+++ HW/Controllers/MoneyController.cs
@@ -1,18 +1,48 @@
 ﻿using HW.Models.ViewModels;
+using HW.Repository;
+using HW.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HW.Models;
 
 namespace HW.Controllers
 {
     public class MoneyController : Controller
     {
+        private readonly HWService _hwService;
+        private readonly UnitOfWork _unitOfWork;
+
+        /// <summary>
+        /// 建構子注入
+        /// </summary>
+        public MoneyController()
+        {
+            _unitOfWork = new UnitOfWork();
+            _hwService = new HWService(_unitOfWork);
+        }
+
         public ActionResult Index()
         {
-           
+            ViewBag.category = _hwService.GetCategoryItems();
 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(MoneyViewModels model)
+        {
+            if (ModelState.IsValid)
+            {
+                _hwService.Add(model);
+
+                _unitOfWork.Commit();
+
+                return RedirectToAction("Index");
+            }
             return View();
         }
 
@@ -21,20 +51,33 @@ namespace HW.Controllers
         {
             List<MoneyViewModels> moneyList = new List<MoneyViewModels>();
 
-            for (int i = 0; i < 50; i++)
-            {
-                MoneyViewModels money = new MoneyViewModels
-                {
-                    pageNo = (i + 1).ToString(),
-                    date = DateTime.Now.AddDays(-i),
-                    category = "支出",
-                    amount = (i+1) * 100
-                };
+            // 第一次作業假資料
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    MoneyViewModels money = new MoneyViewModels
+            //    {
+            //        pageNo = (i + 1).ToString(),
+            //        date = DateTime.Now.AddDays(-i),
+            //        category = "支出",
+            //        amount = (i+1) * 100
+            //    };
 
-                moneyList.Add(money);
-            }
+            //    moneyList.Add(money);
+            //}
+            IEnumerable<AccountBook> accbook = _hwService.Lookup();
 
-            return View(moneyList);
+            int i = 0;
+
+            var data = from m in accbook
+                       select new MoneyViewModels
+                       {
+                           pageNo = (++i).ToString(),
+                           amount = m.Amounttt,
+                           category = m.Categoryyy == 0 ? "支出" : "收入",
+                           date = m.Dateee
+                       };
+
+            return View(data);
         }
     }
 }
--- HW/Models/ViewModels/MoneyViewModels.cs
+++ HW/Models/ViewModels/MoneyViewModels.cs
@@ -16,20 +16,30 @@ namespace HW.Models.ViewModels
         /// <summary>
         /// 類別
         /// </summary>
-        [Display(Name="類別")]
+
+        [Display(Name = "類別")]
         public string category { get; set; }
 
         /// <summary>
         /// 日期
         /// </summary>
-        [Display(Name= "日期")]
+        [Display(Name = "日期")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime date { get; set; }
 
         /// <summary>
         /// 金額
         /// </summary>
-        [Display(Name= "金額")]
+        [Range(0, int.MaxValue, ErrorMessage = "輸入金額太大")]
+        [Display(Name = "金額")]
         public int amount { get; set; }
+
+        /// <summary>
+        /// 備註
+        /// </summary>
+        [Required(ErrorMessage = "test")]
+        [StringLength(200)]
+        [Display(Name = "備註")]
+        public string remark { get; set; }
     }
 }
